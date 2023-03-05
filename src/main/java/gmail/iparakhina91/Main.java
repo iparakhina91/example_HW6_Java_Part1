d3e2bbc75e5624d1c8df809ced09208ba321b15f
Irina <iparakhina91@gmail.com>
2023-03-05 19:06:47 +0300

    added commands in main test file

--- src/main/java/gmail/iparakhina91/Main.java
+++ src/main/java/gmail/iparakhina91/Main.java
@@ -3,52 +3,45 @@ package gmail.iparakhina91;
 public class Main {
     public static void main(String[] args) {
 
-        // Математические операторы
+        // Арифметические операторы
+        int a = 20;
+        int b = 30;
+        int c = 70;
         // сложение
-        int result1 = 1 + 1;
-        System.out.println("### result1: " + result1);
+        System.out.println("a + b = " + (a + b));
         // вычитание
-        int result2 = 10 - 5;
-        System.out.println("### result2: " + result2);
+        System.out.println("a - b = " + (a - b));
         // умножение
-        int result3 = 2 * 3;
-        System.out.println("### result3: " + result3);
+        System.out.println("a * b = " + (a * b));
         // деление
-        int result4 = 100 / 10;
-        System.out.println("### result4: " + result4);
-        // остаток от деления
-        int result5 = 35 % 18;
-        System.out.println("### result5: " + result5);
+        System.out.println("b / a = " + (b / a));
+        // получение остатка от деления
+        System.out.println("b % a = " + (b % a));
+        // инкремент (с разным расположением ++)
+        System.out.println("a++ = " + (a++));
+        System.out.println("++a = " + (++a));
+        // декремент (с разным расположением --)
+        System.out.println("a-- = " + (a--));
+        System.out.println("--a = " + (--a));
+
+        // примеры на приоритеты арифметических операторов
+        System.out.println("a + b * с = " + (a + b * c));
+        System.out.println("(a + b) * с = " + ((a + b) * c));
+        System.out.println("a - c / b = " + (a - c / b);
+        System.out.println("(c - a) / b = " + ((c - a) / b);
+        System.out.println("a + c % b = " + (a + c % b);
 
         // Логические операторы
-        // и (сокращенное)
-        int a = 4, b = 5;
-        if (a > 3 && b > 4) {
-            System.out.println("### result6: true ");
-        } else {
-            System.out.println("### result6: false ");
-        }
-        // или (сокращенное)
-        int c = 2, d = 8;
-        if (c > 5 || d > 3) {
-            System.out.println("### result7: true ");
-        } else {
-            System.out.println("### result7: false ");
-        }
-        // отрицание
         boolean e = true;
-        System.out.println("### result8: " + !e);
-        // двойное отрицание
-
-        boolean f = true;
-        System.out.println("### result9: " + !!f);
+        boolean f = false;
+        // и (сокращенное)
+        System.out.println("e && f = " + (e&&f));
+        // или (сокращенное)
+        System.out.println("e || f = " + (e||f));
+        // не
+        System.out.println("! (e && f) = " + (!(e&&f)));
 
         // Переполнение при вычислениях
-        byte h = Byte.MAX_VALUE;
-        byte t = 1;
-        System.out.println("### result10 " + (h + t));
 
-        int s = Integer.MAX_VALUE;
-        System.out.println("### result11 " + (s + 1));
     }
 }
